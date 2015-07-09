c0724c861444ad16b94a4f3f4b99373bf237f3fa
kowonsik <kows17710@gmail.com>
2015-07-09 14:04:52 +0900

    Update setup_code.sh

--- setup/setup_code.sh
+++ setup/setup_code.sh
@@ -4,16 +4,13 @@
 if [ $1 = 'up' ]; then
     echo "... updating"
     cd
-    cd devel/Logger
     svn up --force
     cd
     
 elif [ $1 = 'co' ]; then
     echo "... installing"
     cd
-    mkdir Logger/
-    cd Logger/
-    svn co http://github.com/jeonghoonkang/BerePi/Logger/trunk Logger/  
+    svn co http://github.com/jeonghoonkang/BerePi/Logger/trunk devel/Logger/  
     svn co svn://125.7.128.53/danalytics --username=tinyos
     cd
 else
