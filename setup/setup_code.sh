71513b441a254ad960a9b65f760f4dfb8b9f3980
kowonsik <kows17710@gmail.com>
2015-07-09 18:23:35 +0900

    Update setup_code.sh

--- setup/setup_code.sh
+++ setup/setup_code.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #Author: jeonghoonkang http://github.com/jeonghoonkang
 
+export gitdnpath='https://raw.githubusercontent.com/kowonsik/RPiLogger/master/sw'
+
+cd devel/Logger
+
 if [ $1 = 'up' ]; then
     echo "... updating"
     cd
@@ -10,8 +14,8 @@ if [ $1 = 'up' ]; then
 elif [ $1 = 'co' ]; then
     echo "... installing"
     cd
-    svn co http://github.com/kowonsik/RPiLogger/ devel/Logger/  
-    svn co svn://125.7.128.53/danalytics --username=tinyos
+    wget 'http://github.com/kowonsik/RPiLogger/sect_ttyUSB0.py devel/Logger/
+    #svn co svn://125.7.128.53/danalytics --username=tinyos
     cd
 else
    echo "... do notihing for code install / update"
