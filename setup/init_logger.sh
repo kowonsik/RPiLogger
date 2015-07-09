2b32d4647e8922a57d3c529f11a6675a1d5bd6c1
kowonsik <kows17710@gmail.com>
2015-07-09 12:47:32 +0900

    Create init_logger.sh

---
+++ setup/init_logger.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Author: jeonghoon.kang@gmail.com
+# Modification : kows17710@gmail.com
+
+echo "Help: source init_logger.sh co <-- checkout" 
+echo "      source init_logger.sh up <-- update" 
+
+export gitdnpath='https://raw.githubusercontent.com/kowonsik/RPiLogger/master/setup'
+
+if [ $1 = 'co' ]; 
+then
+    echo ".... checking out"
+    wget -N $gitdnpath/setup_shell.sh
+    wget -N $gitdnpath/setup_apt.sh
+    wget -N $gitdnpath/setup_code.sh
+    source ./setup_apt.sh
+    source ./setup_shell.sh
+    source ./setup_code.sh co
+elif [ $1 = 'up' ]; 
+then
+    echo ".... update"
+    ./setup_code.sh up
+else
+    echo ".... no action... please add argument "
+fi
+
+unset gitdnpath
+echo ".... ending init.sh"
