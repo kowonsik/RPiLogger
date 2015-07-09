bde8f62f090b32bebc10a18888142d543effdd5e
kowonsik <kows17710@gmail.com>
2015-07-09 21:15:36 +0900

    Update setup_code.sh

--- setup/setup_code.sh
+++ setup/setup_code.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 #Author: jeonghoonkang http://github.com/jeonghoonkang
 
-cd devel/Logger
-
-git clone 'http://github.com/kowonsik/RPiLogger.git
 
 if [ $1 = 'up' ]; then
     echo "... updating"
-    cd
-    svn up --force
-    cd
+    
+    git commit -m "by wonsik"
+    
+    git push -u origin master
     
 elif [ $1 = 'co' ]; then
     echo "... installing"
-    cd
-    wget 'http://github.com/kowonsik/RPiLogger/sect_ttyUSB0.py devel/Logger/
-    #svn co svn://125.7.128.53/danalytics --username=tinyos
-    cd
+    
+    git pull -u origin master
+    
 else
    echo "... do notihing for code install / update"
 fi
